3c68f48e20dbeeffde56862551487e64f1b68e43
JoseNaime <josep.naimeg@gmail.com>
2021-03-27 19:31:21 -0600

    feat(Comp): Play List

---
+++ src/Components/PlayList/PlayList.jsx
@@ -0,0 +1,14 @@
+import './PlayList.css'
+import React from 'react';
+
+export class PlayList extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input value="New Playlist"/>
+
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
