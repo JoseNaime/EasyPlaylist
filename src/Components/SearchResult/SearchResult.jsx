39f05be490c17bd75f77d71bdde025eb2c59670c
JoseNaime <josep.naimeg@gmail.com>
2021-03-27 19:22:20 -0600

    feat(Comp): Search Result

---
+++ src/Components/SearchResult/SearchResult.jsx
@@ -0,0 +1,13 @@
+import './SearchResult.css';
+import React from 'react';
+
+export class SearchResult extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+
+            </div>
+        )
+    }
+}
