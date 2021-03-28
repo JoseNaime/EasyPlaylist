0fdcbd5cb8bb7079b45bc4fbe0a91439934757b5
JoseNaime <josep.naimeg@gmail.com>
2021-03-27 19:18:08 -0600

    feat(Comp): SearchBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {SearchBar} from "./Components/SearchBar/SearchBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+            <SearchBar/>
+          <div className="App-playlist">
+
+          </div>
+        </div>
+      </div>
   );
 }
 
