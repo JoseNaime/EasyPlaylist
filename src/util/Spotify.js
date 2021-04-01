562a478c08482684ab64215fbabc50d9ea1675b9
JoseNaime <josep.naimeg@gmail.com>
2021-04-01 12:52:04 -0600

    feat: Style changed and web name

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = 'ec0940782c8a4fd0b3f65ecd834f8daa'
-const redirectUri = 'https://josenjamming.netlify.app';
+const redirectUri = 'https://easyplaylist.netlify.app';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
